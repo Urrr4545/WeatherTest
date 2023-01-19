7f7bf6e86220f9c3044dfb308ceb4de5897d7a3f
Urrr4545 <ppoewn@gmail.com>
2023-01-19 13:35:57 +0900

    init project

---
+++ app/src/main/java/com/urrr4545/weathertest/presentation/ui/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.urrr4545.weathertest.presentation.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.urrr4545.weathertest.R
+import com.urrr4545.weathertest.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/urrr4545/weathertest/presentation/ui/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.urrr4545.weathertest.presentation.ui
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
