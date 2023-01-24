656f4fb5740e2b77766604ee6a57012bd76f82d6
Urrr4545 <ppoewn@gmail.com>
2023-01-24 16:31:34 +0900

    remove - weather api key

--- app/src/main/java/com/urrr4545/weathertest/Constants.kt
+++ app/src/main/java/com/urrr4545/weathertest/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://api.openweathermap.org/"
-        const val WEATHER_API_KEY = "f7b0b84d7a2c301a204cc3df4958e12f"
+        const val WEATHER_API_KEY = ""
         const val GEO_COUNT = 1
         const val WEATHER_TEMP_METRIC = "metric"
     }
